27d95013dd41c674b50c856088f692fe5a84b3b4
kim <kim.hu@gfc.com.tw>
2022-04-08 17:30:43 +0800

    bugfix

--- .vscode/deploy.bat
+++ .vscode/deploy.bat
@@ -1,12 +1,62 @@
-::copy D:\xampp\htdocs\perl\kim_upload.html %~dp0 /V /-Y
 @ECHO OFF
+REM http://steve-jansen.github.io/guides/windows-batch-scripting/part-2-variables.html
 SETLOCAL ENABLEEXTENSIONS
 chcp 65001
-move d:\kim_upload.html
-@REM echo user mis mis0308 > __ftpcmd__.dat
-@REM echo cd /usr/local/apache_1.3.33/htdocs/frontend/docd400w/  >> __ftpcmd__.dat
-@REM echo put %1 >> __ftpcmd__.dat
-@REM echo bye >> __ftpcmd__.dat
-@REM @ECHO ON
-@REM ftp  -n -v -s:__ftpcmd__.dat 192.6.3.8
-@REM del __ftpcmd__.dat /Q
+SET USERNAME=defaultUserName
+SET PASSWORD=defaultPassword
+
+IF "%2" == "" GOTO STOP
+
+IF "%2" == "31" (
+    SET REMOTE_HOST=192.6.3.1
+    SET REMOTE_PATH=/backup/mistest
+)
+IF "%2" == "38help" (
+    SET REMOTE_HOST=192.6.3.8
+    SET REMOTE_PATH=/usr/local/apache/htdocs/gfchelp
+)
+IF "%2" == "38" (
+    SET REMOTE_HOST=192.6.3.8
+    SET REMOTE_PATH=/usr/local/apache/htdocs/%5
+)
+IF "%2" == "38frontend" (
+    SET REMOTE_HOST=192.6.3.8
+    SET REMOTE_PATH=/usr/local/apache/htdocs/frontend/%5
+)
+
+IF "%2" == "WIO" (
+    IF "%4" == "wio" (
+        start "" "http://192.6.3.8/gfcdb/apb2/?MIval=/APB20/apb_edit_Tag.html&ID=%1&project=%3&obj_type=Dynamic+Tag"
+    ) ELSE (
+        start "" "http://192.6.3.8/gfcdb/apb2/?MIval=/APB20/apb_edit_Page.html&ID=%1&path=/gfc&extension=%4&project=%3&obj_type=AppPage"
+    )
+    GOTO STOP
+)
+IF "%3" == "Download" SET WORK=get %4 %1
+IF "%3" == "Upload" SET WORK=put %1
+
+
+echo open %REMOTE_HOST%>> temp.txt
+echo %USERNAME%>> temp.txt
+echo %PASSWORD%>> temp.txt
+echo cd %REMOTE_PATH%>> temp.txt
+echo binary>> temp.txt
+
+IF "%3" == "Uploads" (
+   echo mkdir css>> temp.txt
+   echo cd css>> temp.txt
+   echo mput %1css\*>> temp.txt
+   echo cd ..>> temp.txt
+   echo mkdir js>> temp.txt
+   echo cd js>> temp.txt
+   echo mput %1js\*>> temp.txt
+   echo cd ..>> temp.txt
+   SET WORK=mput %1*
+)
+
+echo %WORK%>> temp.txt
+echo quit>> temp.txt
+@ECHO ON
+ftp -v -i -s:temp.txt
+del temp.txt
+:STOP
--- extension.js
+++ extension.js
@@ -2,6 +2,17 @@
 const vscode = require('vscode');
 const process = require('child_process');
 const stderr = require('./lib/stdout_vaild');
+const fs = require('fs');
+
+const extPath = vscode.extensions.getExtension('gfc.demo').extensionPath+'\\.vscode';
+const drive = extPath.slice(0,2)
+
+const batPath = `${drive} && cd ${extPath} && deploy.bat`;
+let inputBoxTitle = '',
+		inputBoxPlaceHolder = '',
+		inputBoxExtension = '',
+		promptText = '',
+		batCommand = ``;
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -10,22 +21,12 @@ function activate(context) {
 
 	console.info('插件已啟動');
 
-	let userName = '',
-		  password = '';
-	/**
-	 * @param {vscode.TextDocument} t 依照TextDocument內容更改語言
-	 */
-	const setEncoding = (t) =>{
-		if(t.getText().match(/<\?MI/i)){
-			vscode.languages.setTextDocumentLanguage(t,'wio');
-			return;
-			}
-		if(t.getText().match(/<!DOCTYPE html>/i)){
-			vscode.languages.setTextDocumentLanguage(t,'html');
-			return;
+	//檢查有沒有設定過帳密
+	fs.readFile(extPath+'\\deploy.bat',{encoding:'utf-8'},(e,buffer)=>{
+		if(buffer.includes('defaultUserName')||buffer.includes('defaultPassword')){
+			setUserAndPass(buffer);
 		}
-	}
-
+	})
 	//插件啟動時(只執行一次)
 	setEncoding(vscode.window.activeTextEditor.document);
 
@@ -38,135 +39,18 @@ function activate(context) {
 			setEncoding(t);
 	})
 
-	const batPath = `cd %userprofile%\\.vscode\\extensions && deploy.bat`;
-	let inputBoxTitle = '',
-			inputBoxPlaceHolder = '',
-			inputBoxExtension = '',
-			promptText = '',
-			batCommand = ``;
-
-/**
- * @param {vscode.TextEditor} t
- * @param {String} command
- */
-	const logInFn = (command,t) =>{
-		if(!userName||!password){
-			vscode.commands.executeCommand('logIn').then(()=>{
-				if(!userName||!password){
-					return;
-				}
-				return executeCommand(command,t);
-			});
-		}
-		if(userName||password){
-		  return executeCommand(command,t);
-		}
-	}
-/**
- * @param {vscode.TextEditor} t
- * @param {String} command
- */
-	const executeCommand = (command,t) =>{
-		
-		let file = t.document.fileName,
-				fileArr = file.split('\\').reverse(),
-				fileExtname = fileArr[0].replace(/.*\.(.+)/,'$1'),
-				fileBaseName = fileArr[0],
-				relativeFileDirname = fileArr[1],
-				relativeFilePath = file.replace(/(.+\\).+/,'$1'),
-				fileBasenameNoExtension = fileBaseName.replace(/(.*)\..+/,'$1');
-
-		//fileExtname = t.document.languageId(The selected language mode not file extension)
-		//${file}=c:\root\your-project\folder\file.ext
-		//${fileExtname}=ext
-		//${fileBaseName}=file.ext
-		//${relativeFileDirname}=folder
-		//${relativeFilePath}=c:\root\your-project\folder\
-		//${fileBasenameNoExtension}=file
-		
-		//InputBox參數 https://code.visualstudio.com/api/references/vscode-api#InputBoxOptions
-		if(command == 'download'){
-			vscode.window.showInputBox({
-				ignoreFocusOut:true,
-				title: inputBoxTitle,
-				value: fileBaseName,
-				placeHolder: inputBoxPlaceHolder,
-				prompt:promptText
-			}).then(input=>{
-				if(!input){
-					vscode.window.showInformationMessage('已取消');
-					return;
-				}
-				input = input.slice(-inputBoxExtension.length)==inputBoxExtension?input:input+inputBoxExtension;
-				relativeFilePath = relativeFilePath+input;
-				process.exec(`${batPath} ${relativeFilePath} ${batCommand} ${input} ${relativeFileDirname} ${userName} ${password}`,(error,stdout)=>{
-					console.log(stdout)
-					if(error){
-						vscode.window.showErrorMessage(error.message);
-						return;
-					}
-				
-					if(stderr(stdout)[0]){
-						vscode.window.showErrorMessage(stderr(stdout)[1]);
-						return;
-					}
-					vscode.workspace.openTextDocument(vscode.Uri.file(relativeFilePath)).then((doc)=>{	
-						vscode.window.showTextDocument(doc);
-					});
-				});
-			});
-		}
-
-		if(command == 'upload'){
-			//workSpaceRootName = vscode.workspace.workspaceFolders[0].name
-			//workSpaceRootPath = vscode.workspace.workspaceFolders[0].uri.fsPath
-			if(batCommand == `38frontend Uploads`){
-				file = vscode.workspace.workspaceFolders[0].uri.fsPath+'\\dist\\';
-				relativeFileDirname = vscode.workspace.workspaceFolders[0].name;
-			}
-			process.exec(`${batPath} ${file} ${batCommand} ${fileBaseName} ${relativeFileDirname} ${userName} ${password}`,(error,stdout)=>{
-				console.log(stdout);
-				if(error){
-					vscode.window.showErrorMessage(error.message);
-					return;
-				}
-			
-				if(stderr(stdout)[0]){
-					vscode.window.showErrorMessage(stderr(stdout)[1]);
-					return;
-				}
-				vscode.window.showInformationMessage('上傳成功');
-			});
-		}
-
-		if(command == 'open'){
-			process.exec(`${batPath} ${fileBasenameNoExtension} ${batCommand} ${relativeFileDirname} ${fileExtname}`,(error,stdout)=>{
-				if(error){
-					vscode.window.showErrorMessage(error.message);
-					return;
-				}
-			
-				if(stderr(stdout)[0]){
-					vscode.window.showErrorMessage(stderr(stdout)[1]);
-					return;
-				}
-				vscode.window.showInformationMessage('開啟成功');
-			});
-		}
-	};
-
 	let download4gl = vscode.commands.registerTextEditorCommand('download4gl', (textEditor) => {
 		inputBoxTitle = '下載4gl';
 		inputBoxPlaceHolder = '4GL檔名(可不加.4gl)';
 		inputBoxExtension = '.4gl';
 		promptText = '從3.1的mistest下載4gl';
 		batCommand = `31 Download`;
-		logInFn('download',textEditor);
+		executeCommand('download',textEditor);
   });
 
 	let upload4gl = vscode.commands.registerTextEditorCommand('upload4gl', (textEditor) => {
 		batCommand = `31 Upload`;
-		logInFn('upload',textEditor);
+		executeCommand('upload',textEditor);
 	});
 
 	let downloadNewEraHelp = vscode.commands.registerTextEditorCommand('downloadNewEraHelp', (textEditor) => {
@@ -175,12 +59,12 @@ function activate(context) {
 		inputBoxExtension = '.htm';
 		promptText = '從3.8的htdocs/gfchelp下載htm';
 		batCommand = `38help Download`;
-		logInFn('download',textEditor);
+		executeCommand('download',textEditor);
 	});
 
 	let uploadNewEraHelp = vscode.commands.registerTextEditorCommand('uploadNewEraHelp', (textEditor) => {
 		batCommand = `38help Upload`;
-		logInFn('upload',textEditor);
+		executeCommand('upload',textEditor);
 	});
 
 	let downloadFile38 = vscode.commands.registerTextEditorCommand('downloadFile3.8', (textEditor) => {
@@ -189,45 +73,27 @@ function activate(context) {
 		inputBoxExtension = '';
 		promptText = '從3.8的 htdocs/(你的上層資料夾) 下載檔案(須加上完整副檔名) 例：htdocs/docd400w';
 		batCommand = `38 Download`;
-		logInFn('download',textEditor);
+		executeCommand('download',textEditor);
 	});
 
 	let uploadFile38 = vscode.commands.registerTextEditorCommand('uploadFile3.8', (textEditor) => {
 		batCommand = `38 Upload`;
-		logInFn('upload',textEditor);
+		executeCommand('upload',textEditor);
 	});
 
 	let openWIOPage = vscode.commands.registerTextEditorCommand('openWIOPage', (textEditor) => {
 		batCommand = `WIO`;
-		logInFn('open',textEditor);
+		executeCommand('open',textEditor);
 	});
 	
 	let uploadfrontend = vscode.commands.registerTextEditorCommand('uploadfrontend', (textEditor) => {
 		batCommand = `38frontend Uploads`;
-		logInFn('upload',textEditor);
+		executeCommand('upload',textEditor);
   });
 
-	let logIn = vscode.commands.registerCommand('logIn', async () => {
-		await vscode.window.showInputBox({
-			ignoreFocusOut:false,
-			title: '請輸入UserName',
-			value: userName,
-			prompt: '請設定連線至3.1和3.8的UserName'
-		}).then(async val=>{
-			if(!val){
-				return;
-			}
-			userName = val;
-			await vscode.window.showInputBox({
-				ignoreFocusOut:false,
-				title: '請輸入Password',
-				value: password,
-				password: true,
-				prompt: '請設定連線至3.1和3.8的Password'
-			}).then(v=>{
-				password = v;
-				return;
-			})
+	let logIn = vscode.commands.registerCommand('logIn', () => {
+		fs.readFile(extPath+'\\deploy.bat',{encoding:'utf-8'},(e,buffer)=>{
+				setUserAndPass(buffer);
 		})
   });
 
@@ -243,6 +109,148 @@ function activate(context) {
 		logIn
 	);
 }
+
+/**
+ * 
+ * @param {String} buf 
+ */
+const setUserAndPass = async (buf) =>{
+	
+	let userIndexEnd = buf.indexOf('PASSWORD=')-5;
+	let passIndexStart = buf.indexOf('PASSWORD=')+9;
+	let passIndexEnd = buf.indexOf('IF')-3;
+	let userName = '',
+			password = '';
+	await vscode.window.showInputBox({
+		ignoreFocusOut:true,
+		title: '請輸入UserName',
+		prompt: '請設定連線至3.1和3.8的UserName'
+	}).then(inputUserName=>{
+		userName = inputUserName;
+	})
+
+	await vscode.window.showInputBox({
+		ignoreFocusOut:true,
+		title: '請輸入Password',
+		password: true,
+		prompt: '請設定連線至3.1和3.8的Password'
+	}).then(inputPassword=>{
+		password = inputPassword;
+	})
+
+	if(password&&userName){
+		buf = buf.slice(0,151)+userName+buf.slice(userIndexEnd,passIndexStart)+password+buf.slice(passIndexEnd);
+		fs.writeFile(extPath+'\\deploy.bat',buf,e=>{
+			if(e){
+				vscode.window.showErrorMessage(e.message);
+			}else{
+				vscode.window.showInformationMessage('設定成功');
+			}
+		})
+	}else{
+		vscode.window.showInformationMessage('尚未設定帳號密碼');
+	}
+}
+
+/**
+	 * @param {vscode.TextDocument} t 依照TextDocument內容更改語言
+	 */
+ const setEncoding = (t) =>{
+	if(t.getText().match(/<\?MI/i)){
+		vscode.languages.setTextDocumentLanguage(t,'wio');
+		}
+}
+
+/**
+ * @param {vscode.TextEditor} t
+ * @param {String} command
+ */
+ const executeCommand = (command,t) =>{
+		
+	let file = t.document.fileName,
+			fileArr = file.split('\\').reverse(),
+			fileExtname = fileArr[0].replace(/.*\.(.+)/,'$1'),
+			fileBaseName = fileArr[0],
+			relativeFileDirname = fileArr[1],
+			relativeFilePath = file.replace(/(.+\\).+/,'$1'),
+			fileBasenameNoExtension = fileBaseName.replace(/(.*)\..+/,'$1');
+
+	//fileExtname = t.document.languageId(The selected language mode not file extension)
+	//${file}=c:\root\your-project\folder\file.ext
+	//${fileExtname}=ext
+	//${fileBaseName}=file.ext
+	//${relativeFileDirname}=folder
+	//${relativeFilePath}=c:\root\your-project\folder\
+	//${fileBasenameNoExtension}=file
+
+	//InputBox參數 https://code.visualstudio.com/api/references/vscode-api#InputBoxOptions
+	if(command == 'download'){
+		vscode.window.showInputBox({
+			ignoreFocusOut:true,
+			title: inputBoxTitle,
+			value: fileBaseName,
+			placeHolder: inputBoxPlaceHolder,
+			prompt:promptText
+		}).then(input=>{
+			if(!input){
+				vscode.window.showInformationMessage('已取消');
+				return;
+			}
+			input = input.slice(-inputBoxExtension.length)==inputBoxExtension?input:input+inputBoxExtension;
+			relativeFilePath = relativeFilePath+input;
+			process.exec(`${batPath} ${relativeFilePath} ${batCommand} ${input} ${relativeFileDirname}`,(error,stdout)=>{
+				if(error){
+					vscode.window.showErrorMessage(error.message);
+					return;
+				}
+			
+				if(stderr(stdout)[0]){
+					vscode.window.showErrorMessage(stderr(stdout)[1]);
+					return;
+				}
+				vscode.workspace.openTextDocument(vscode.Uri.file(relativeFilePath)).then((doc)=>{	
+					vscode.window.showTextDocument(doc);
+				});
+			});
+		});
+	}
+
+	if(command == 'upload'){
+		//workSpaceRootName = vscode.workspace.workspaceFolders[0].name
+		//workSpaceRootPath = vscode.workspace.workspaceFolders[0].uri.fsPath
+		if(batCommand == `38frontend Uploads`){
+			file = vscode.workspace.workspaceFolders[0].uri.fsPath+'\\dist\\';
+			relativeFileDirname = vscode.workspace.workspaceFolders[0].name;
+		}
+		process.exec(`${batPath} ${file} ${batCommand} ${fileBaseName} ${relativeFileDirname}`,(error,stdout)=>{
+			if(error){
+				vscode.window.showErrorMessage(error.message);
+				return;
+			}
+		
+			if(stderr(stdout)[0]){
+				vscode.window.showErrorMessage(stderr(stdout)[1]);
+				return;
+			}
+			vscode.window.showInformationMessage('上傳成功');
+		});
+	}
+
+	if(command == 'open'){
+		process.exec(`${batPath} ${fileBasenameNoExtension} ${batCommand} ${relativeFileDirname} ${fileExtname}`,(error,stdout)=>{
+			if(error){
+				vscode.window.showErrorMessage(error.message);
+				return;
+			}
+		
+			if(stderr(stdout)[0]){
+				vscode.window.showErrorMessage(stderr(stdout)[1]);
+				return;
+			}
+			vscode.window.showInformationMessage('開啟成功');
+		});
+	}
+};
 
 function deactivate() {}
 
